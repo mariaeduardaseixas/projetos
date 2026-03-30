b083bedfdb5735c37604deb941335c617bc5506e
MARIA EDUARDA SIPRIANO SEIXAS <saileseixas@gmail.com>
2026-03-30 11:03:00 -0300

    Refactor calculator logic and add parity check

--- calculadora.py
+++ calculadora.py
@@ -29,7 +29,7 @@
 # prferência de escolha - adicione com input
 escolha = input("Digite o número da operação (1/2/3/4/5): ")
 # in - subconjunto em python como listas,tuplas
-if escolha in ('1', '2', '3', '4','5','6'):
+if escolha in ('1', '2', '3', '4','5','6','7'):
 
     num1 = float(input("Digite o primeiro número: "))
     num2 = float(input("Digite o segundo número: "))
@@ -44,12 +44,17 @@ if escolha in ('1', '2', '3', '4','5','6'):
     elif escolha == '3':
             print(f"Resultado: {num1 * num2}")
     elif escolha == '4':
-            if num2 != 0:
+             num2 != 0:
                 print(f"Resultado: {num1 / num2}")
     elif escolha == '5':
-        print(f"resultado: {num1 ** num2}")
+             print(f"resultado: {num1 ** num2}")
     elif escolha == '6';
         print(pow(num1, num2))
+elif escolha == '7':
+    num1 % 2:
+    print("Par")
+else: 
+    print("Ímpar")
         
     else:
                 print("Erro: Divisão por zero! número ímpar")
