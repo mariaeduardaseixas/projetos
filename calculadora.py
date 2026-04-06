41c8f96631d4937c3091618b353bec8f492f97be
MARIA EDUARDA SIPRIANO SEIXAS <saileseixas@gmail.com>
2026-04-06 10:10:17 -0300

    Refactor calculator logic and improve input handling

--- calculadora.py
+++ calculadora.py
@@ -27,39 +27,55 @@
 
 
 # prferência de escolha - adicione com input
-escolha = input("Digite o número da operação (1/2/3/4/5): ")
-# in - subconjunto em python como listas,tuplas
-if escolha in ('1', '2', '3', '4','5','6','7'):
+import math
+print("CALCULADORA")
+print("1 - Adição\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Potência\n6 - Raiz qudrada\n7 - Número par\n8 - Número ímpar")
 
+# prferência de escolha - adicione com input
+import math
+print("CALCULADORA")
+print("1 - Adição\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Potência\n6 - Raiz qudrada\n7 - Número par\n8 - Número ímpar")
+
+escolha = input("Digite o número da operação (1-8): ")
+# in - subconjunto em python como listas,tuplas
+if escolha in ('1', '2', '3', '4','5'):
     num1 = float(input("Digite o primeiro número: "))
     num2 = float(input("Digite o segundo número: "))
+elif escolha in ['6','7','8']:
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = None
 
-        
-    if escolha == '1':
-            print(f"Resultado: {num1 + num2}")
-        
-    elif escolha == '2':
-            print(f"Resultado: {num1 - num2}")
-        
-    elif escolha == '3':
-            print(f"Resultado: {num1 * num2}")
-    elif escolha == '4':
-             num2 != 0:
-                print(f"Resultado: {num1 / num2}")
-    elif escolha == '5':
-             print(f"resultado: {num1 ** num2}")
-    elif escolha == '6';
-        print(pow(num1, num2))
-elif escolha == '7':
-    num1 % 2:
-    print("Par")
-else: 
-    print("Ímpar")
-        
-    else:
-                print("Erro: Divisão por zero! número ímpar")
-else:
-        print("Opção inválida!")
+match escolha:
+    case '1':
+        print(f"num1 + num2 = : {num1 + num2}")
+    case '2':
+        print(f"num1 - num2 = : {num1 - num2}")
+    case '3':
+        print(f"num1 * num2 = : {num1 * num2}")
+    case '4':
+        if num2 != 0:
+            print(f"num1 / num2 = : {num1 / num2}")
+        else:
+            print(f"Erro: Não é opssível dividir por zero")
+    case '5':
+        print(f"num1 ^ num2 = : {pow(num1, num2)}")
+    case '6':
+        if num1 >= 0:
+            print(f"Raiz quadrada = {math.sqrt(num1)}")
+        else: 
+            print("Erro: raiz quadrada de número negativo: ")
+    case '7':
+        if num1 % 2 == 0:
+            print(f"O número {num1} é par!")
+        else: 
+            print(f"O número {num1} não é par ")
+    case '8':
+        if num1 % 2 == 1:
+           print(f"O número {num1} é ímpar!") 
+        else:
+            print(f"O número {num1} não é ímpar")
+    case '9':
+        print("Opção inválida")
 
 # else - erro e opção inválida
 
